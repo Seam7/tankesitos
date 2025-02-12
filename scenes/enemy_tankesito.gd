5687cb6432f9d733a4d1643915ce2bf62e6cb699
Samuel Alvarez <seam.2194@gmail.com>
2025-02-11 22:24:24 -0600

    Add random movement to enemies

--- scenes/enemy_tankesito.gd
+++ scenes/enemy_tankesito.gd
@@ -13,6 +13,8 @@ const PROJECTILE_SPEED = 1000
 const PROJECTILE_FIRE_RATE = 1
 
 var can_fire = true
+var should_change_direction = true
+var direction = Vector2()
 
 # speed in pixels/sec
 
@@ -36,30 +38,58 @@ func fire():
 	await get_tree().create_timer(PROJECTILE_FIRE_RATE).timeout
 	can_fire = true
 	
-func _process(delta: float): 
-	pass
-	#if Input.is_action_pressed("ui_select") and can_fire:
-		#fire()
+func _process(delta: float):
+	if(should_change_direction):
+		should_change_direction = false
+		var random_direction = RandomNumberGenerator.new().randi_range(1, 4)
+		print(random_direction % 2 == 0)
+		if(random_direction == 1):
+			direction = move("right")
+		elif(random_direction == 2):
+			direction = move("up")
+		elif(random_direction == 3):
+			direction = move("left")
+		else:
+			print("in else")
+			direction = move("down")
+		await get_tree().create_timer(1.3).timeout
+		should_change_direction = true
 	
-
+func move(vector: String) -> Vector2:
+	var direction = Vector2()
+	if(vector == "left"):
+		rotation_degrees = 270
+		direction.x -= 1
+	elif(vector == "right"):
+		rotation_degrees = 90
+		direction.x += 1
+	elif(vector == "down"):
+		rotation_degrees = 180
+		direction.y += 1
+	else:
+		rotation_degrees = 0
+		direction.y -= 1
+	return direction
+	
 func _physics_process(delta):
 	if raycast.is_colliding():
 		var body = raycast.get_collider()
 		if evaluate_targets(body) and can_fire:
 			fire()
-	var direction = Vector2()
-	if Input.is_action_pressed("ui_up"):
-		rotation_degrees = 0
-		direction.y -= 1
-	elif Input.is_action_pressed("ui_down"):
-		rotation_degrees = 180
-		direction.y += 1
-	elif Input.is_action_pressed("ui_left"):
-		rotation_degrees = 270
-		direction.x -= 1
-	elif Input.is_action_pressed("ui_right"):
-		rotation_degrees = 90
-		direction.x += 1
+	#var direction = Vector2()
+	#if Input.is_action_pressed("ui_up"):
+		#rotation_degrees = 0
+		#direction.y -= 1
+	#elif Input.is_action_pressed("ui_down"):
+		#rotation_degrees = 180
+		#direction.y += 1
+	#elif Input.is_action_pressed("ui_left"):
+		#rotation_degrees = 270
+		#direction.x -= 1
+	#elif Input.is_action_pressed("ui_right"):
+		#rotation_degrees = 90
+		#direction.x += 1
+	#var direction = move("right")
 	
 # normalize the directional movement
 	direction = direction.normalized()
